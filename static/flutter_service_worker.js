7ac7fa30a382e274053fa3f921ed16be8ebb8b6d
Oğuz Kurt <kurt.oguz@outlook.com>
2022-02-13 21:42:26 +0300

    new version of flutter web app deployed

--- static/flutter_service_worker.js
+++ static/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "cac5958d61f3dd6303c9bbea7a560fda",
-"index.html": "46916b0fb1df165dbedde8389d9af520",
-"/": "46916b0fb1df165dbedde8389d9af520",
+"index.html": "f352ac17e8a9ce30d66e992b8359c9ff",
+"/": "f352ac17e8a9ce30d66e992b8359c9ff",
 "main.dart.js": "fe6ebc98f4eaab049dd7ff2d4f091f02",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -17,11 +17,7 @@ const RESOURCES = {
 "assets/NOTICES": "16058e535c19b138aa3b97cab15645f8",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"canvaskit/canvaskit.js": "43fa9e17039a625450b6aba93baf521e",
-"canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
-"canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
-"canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498"
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1"
 };
 
 // The application shell files that are downloaded before a service worker can
